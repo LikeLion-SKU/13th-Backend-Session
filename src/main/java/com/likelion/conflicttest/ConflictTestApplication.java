ef2cbc268a2b0440727894dc7c6aeed67c49bfbb
shinchaerin79 <shinchaerin79@gmail.com>
2025-03-28 13:41:44 +0900

    :tada:Start: ì 새 프로젝트 생성

---
+++ src/main/java/com/likelion/conflicttest/ConflictTestApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.conflicttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConflictTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConflictTestApplication.class, args);
+    }
+
+}
