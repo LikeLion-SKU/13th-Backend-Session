c998ed8d88661df3d9edc0fd7a68200d0b8b57ad
Ksieon <keumsiun0503@gmail.com>
2025-03-31 20:46:26 +0900

    :sparkles: Hello world, SQL연동

---
+++ src/main/java/com/example/demo/week2/helloworld/StudentController.java
@@ -0,0 +1,22 @@
+package com.example.demo.week2.helloworld;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class StudentController {
+
+  private final StudentRepository studentRepository;
+
+  public StudentController(StudentRepository studentRepository) {
+    this.studentRepository = studentRepository;
+  }
+
+  @GetMapping("/students")
+  public List<Student> getStudents() {
+    return studentRepository.findAll();
+  }
+}
---
+++ src/main/java/com/example/demo/week2/helloworld/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.week2.helloworld;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
