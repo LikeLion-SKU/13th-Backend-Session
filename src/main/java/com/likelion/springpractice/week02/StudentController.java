6f05bd5fbdde96764ff2b76afa45edb3bc0b4311
YoonHeeJun <unijun0109@gmail.com>
2025-04-01 13:24:29 +0900

    :sparkles:Feat: 2주차 실습 과제 업로드

---
+++ src/main/java/com/likelion/springpractice/week02/StudentController.java
@@ -0,0 +1,22 @@
+package com.likelion.springpractice.week02;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentController {
+  private final StudentRepository studentRepository;
+
+  public StudentController(StudentRepository studentRepository) {
+    this.studentRepository = studentRepository;
+  }
+
+  @GetMapping("/students")
+  public List<Student> getStudents() {
+    return studentRepository.findAll();
+  }
+}
---
+++ src/main/java/com/likelion/springpractice/week02/StudentRepository.java
@@ -0,0 +1,6 @@
+package com.likelion.springpractice.week02;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
