75a394e7b899a972c70e4b6bbd87d8dffc965221
Dongmyoung Min <zmarzmzm@naver.com>
2025-04-01 16:22:39 +0900

    ✨Feat: Week2

---
+++ src/main/java/com/likelion/springpractice/StudentController.java
@@ -0,0 +1,23 @@
+package com.likelion.springpractice;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class StudentController {
+
+  private final StudentRepository studentRepository;
+
+  public StudentController(StudentRepository studentRepository) {
+    this.studentRepository = studentRepository;
+  }
+
+  @GetMapping("/students")
+  public List<Student> getAllStudents() {
+    return studentRepository.findAll();
+  }
+}
+
