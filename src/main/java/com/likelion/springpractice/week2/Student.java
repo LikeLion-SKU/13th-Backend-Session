f7cfa663f4912b6b1eff5b3be630b1c1cabde471
hyunseoko <kohyun0223@skuniv.ac.kr>
2025-04-01 21:12:44 +0900

    :sparkles: Feat: 2주차 Students Entity

---
+++ src/main/java/com/likelion/springpractice/week2/Student.java
@@ -0,0 +1,30 @@
+package com.likelion.springpractice.week2;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private Long age;
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Long getAge() {
+    return age;
+  }
+}
