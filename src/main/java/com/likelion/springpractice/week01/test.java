b10b9f434a071b05e81314c32ad46d437b87e791
jaeyeon <kasto6329@gmail.com>
2025-04-04 15:10:49 +0900

    🚚Rename: 프로젝트 구조 변경 및 일부 파일 이동

---
+++ src/main/java/com/likelion/springpractice/week01/test.java
@@ -0,0 +1,9 @@
+package com.likelion.springpractice.week01;
+
+public class test {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World! I don't think so");
+  }
+}
+
---
+++ src/main/java/com/likelion/springpractice/week02/HelloController.java
@@ -0,0 +1,16 @@
+package com.likelion.springpractice.week02;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+
+public class HelloController {
+
+  @GetMapping("/hello")
+  public String helloWorld() {
+    return "Hello World!";
+  }
+}
