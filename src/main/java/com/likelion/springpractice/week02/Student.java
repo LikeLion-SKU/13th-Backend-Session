8a0945f604dcb281c64b9f654589e68b28a6956c
pjuyong <imjuyongp@naver.com>
2025-04-09 19:02:51 +0900

    :sparkles: 3주차 추가

--- src/main/java/com/likelion/springpractice/week02/Student.java
+++ src/main/java/com/likelion/springpractice/week02/Student.java
@@ -6,12 +6,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-@Entity
-@Table(name = "students")
+@Entity // 이 클래스가 JPA 엔티티임을 나타냄. 즉, 이 클래스는 DB의 테이블과 매핑됨.
+@Table(name = "students") // 엔티티가 매핑할 테이블 이름을 지정.
 public class Student {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Id // 해당 필드가 기본키 임을 나타낸다.(Id가 테이블의 기본키이다.)
+  @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키 값을 자동으로 생성하도록 설정 (자동 증가)
+
   private Long id;
   private String name;
   private Long age;
--- src/main/java/com/likelion/springpractice/week02/StudentController.java
+++ src/main/java/com/likelion/springpractice/week02/StudentController.java
@@ -5,18 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api")
+@RestController // 클래스가 REST API controller임을 선언
+@RequestMapping("/api") // API 엔드포인트 설정
 public class StudentController {
 
   private final StudentRepository studentRepository;
 
-  public StudentController(StudentRepository studentRepository) {
+  public StudentController(StudentRepository studentRepository) { // 생성자
     this.studentRepository = studentRepository;
   }
 
-  @GetMapping("/students")
+  @GetMapping("/students") // http GET 요청을 처리
   public List<Student> getStudents() {
-    return studentRepository.findAll();
+    return studentRepository.findAll(); // 레포지토리에 있는 모든 요소들이 담긴 리스트를 리턴
   }
 }
