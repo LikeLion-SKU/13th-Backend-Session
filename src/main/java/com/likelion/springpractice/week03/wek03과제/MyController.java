9081ab62b714fab0e06161774385882df31f6ec4
pjuyong <imjuyongp@naver.com>
2025-04-10 20:14:10 +0900

    :sparkle:Feat: 3주차 실습과제

---
+++ src/main/java/com/likelion/springpractice/week03/wek03과제/MyController.java
@@ -0,0 +1,49 @@
+package com.likelion.springpractice.week03.wek03과제;
+
+import com.likelion.springpractice.week02.Student;
+import com.likelion.springpractice.week02.StudentRepository;
+import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 클래스가 REST API controller임을 선언
+@RequestMapping("/api") // API 엔드포인트 설정
+public class MyController {
+
+  private final StudentRepository studentsRepository;
+
+  public MyController(StudentRepository studentsRepository) {
+    this.studentsRepository = studentsRepository;
+  }
+
+  @PostMapping("/studentId") // 학생 추가
+  public String addStudent() {
+    return "add student";
+  }
+/*
+  @GetMapping("students") // 학생 조회
+  public List<Student> getStudents() {
+    return studentsRepository.findAll();
+  }
+*/
+  @PutMapping("put-students") // 학생 전체 수정
+  public String putStudent() {
+    return "update all students";
+  }
+
+  @PatchMapping("patch-students") // 학생 부분 수정
+  public String patchStudnet() {
+    return "update student";
+  }
+
+  @DeleteMapping("delete-student") // 학생 삭제
+  public String deleteStudent() {
+    return "delete student";
+  }
+
+}
