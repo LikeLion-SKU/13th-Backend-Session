53f7aed9f83576ecd7aa32890598f36dd1e1ffd0
simGPT <2023216057@skuniv.ac.kr>
2025-04-11 02:04:37 +0900

    :wrench:Settings:swagger 설정

---
+++ src/main/java/com/likelion/springpractice/week03/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package com.likelion.springpractice.week03;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration //설정 클래스임을 나타낸다
+public class SwaggerConfig {
+
+  @Value("${server.servlet.context-path:}") // 공개가 되면 안되는 값들은 이런식으로 관리 
+  private String contextPath;
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    Server localServer = new Server();
+    localServer.setUrl(contextPath);
+    localServer.setDescription("Local Server");
+
+    return new OpenAPI()
+        .addServersItem(localServer)
+        .addSecurityItem(new SecurityRequirement().addList("bearerAuth")) //JWT의 토큰값을 사용하기 위한 코드
+        .components( // JWT 토큰 사용하기 위한 코드
+            new Components()
+                .addSecuritySchemes(
+                    "bearerAuth",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                )
+        )
+        .info(new Info().title("Swagger API 명세서").version("1.0").description("MySwagger"));
+
+
+  }
+
+  @Bean
+  public GroupedOpenApi customGroupedOpenApi() {
+    return GroupedOpenApi.builder().group("api").pathsToMatch("/**").build();
+  }
+
+  SwaggerConfig() {
+
+  }
+
+}
