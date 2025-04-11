f86640ffe359c8951f06c4ca6e5931365bd45132
chaeelin <chaeee0123@gmail.com>
2025-04-11 18:15:30 +0900

    :sparkles:Add: 3주차 과제 제출

---
+++ src/main/java/com/likelion/springpractice/week03/PostController.java
@@ -0,0 +1,38 @@
+package com.likelion.springpractice.week03;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class PostController {
+  @PostMapping("/create-post")
+  public String createPost() {
+    return "This is create post";
+  }
+
+  @GetMapping("get-post")
+  public String readPost() {
+    return "This is the post";
+  }
+
+  @PatchMapping("update-post")
+  public String updatePatchPost() {
+    return "This is update patch post";
+  }
+
+  @PutMapping("update-post")
+  public String updatePutPost() {
+    return "This is update put post";
+  }
+
+  @DeleteMapping("delete-post")
+  public String deletePost() {
+    return "This is delete post";
+  }
+}
