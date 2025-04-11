cd18420db209e5b645cadd244191e2ade236e9bc
Ksieon <keumsiun0503@gmail.com>
2025-04-11 11:37:38 +0900

    :sparkles:Feat: 3주차 과제 MyController 생성

---
+++ src/main/java/com/likelion/springpractice/week03/MyController.java
@@ -0,0 +1,40 @@
+package com.likelion.springpractice.week03;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class MyController {
+
+    @PostMapping("users")
+    public String createUser() {
+        return "This is create user";
+    }
+
+    @GetMapping("users/{userId}")
+    public String getUsers(@PathVariable long userId) {
+        return "This is get user information by " + userId;
+    }
+
+    @PatchMapping("users/{userId}")
+    public String updatePatchUser(@PathVariable long userId) {
+        return "This is update Patch user by " + userId;
+    }
+
+    @PutMapping("users/{userId}")
+    public String updatePutUser(@PathVariable long userId) {
+        return "This is update Put user by " + userId;
+    }
+
+    @DeleteMapping("users/{userId}")
+    public String deleteUser(@PathVariable long userId) {
+        return "This is delete user by " + userId;
+    }
+}
