c46556aa29012a9083af68325c78074429ed47a7
Ksieon <keumsiun0503@gmail.com>
2025-04-14 19:59:57 +0900

    :recycle:Refactor: 어노테이션 추가

--- src/main/java/com/likelion/springpractice/SpringPracticeApplication.java
+++ src/main/java/com/likelion/springpractice/SpringPracticeApplication.java
@@ -2,12 +2,14 @@ package com.likelion.springpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringPracticeApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(SpringPracticeApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPracticeApplication.class, args);
+    }
 
 }
