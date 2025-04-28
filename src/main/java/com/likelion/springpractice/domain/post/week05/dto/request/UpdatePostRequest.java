1c1f7ac4b3c0de915ad26edcd410308a268f0a3d
Sieon Keum <keumsiun0503@gmail.com>
2025-04-28 22:55:03 +0900

    :sparkles:Feat: 5주차 실습

--- src/main/java/com/likelion/springpractice/domain/post/week05/dto/request/UpdatePostRequest.java
+++ src/main/java/com/likelion/springpractice/domain/post/week05/dto/request/UpdatePostRequest.java
@@ -1,5 +1,14 @@
 package com.likelion.springpractice.domain.post.week05.dto.request;
 
-public class UpdatePostRequest {
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
 
+@Getter
+@Schema(title="UpdatePostRequest: 게시글 수정 요청 DTO")
+public class UpdatePostRequest {
+    @Schema(description = "게시글 제목", example = "5주차 세션")
+    private String title;
+
+    @Schema(description = "게시글 내용", example = "이번주 세션도 화이팅!")
+    private String content;
 }
