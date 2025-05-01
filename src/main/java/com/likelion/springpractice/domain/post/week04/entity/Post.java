a6d69c1503690aa8cb76c3ebd2eff21a0b432fc8
simGPT <2023216057@skuniv.ac.kr>
2025-05-01 22:01:23 +0900

    :tada:Start: 게시글 Entity 생성

---
+++ src/main/java/com/likelion/springpractice/domain/post/week04/entity/Post.java
@@ -0,0 +1,37 @@
+package com.likelion.springpractice.domain.post.week04.entity;
+
+import com.likelion.springpractice.global.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = " post")
+public class Post extends BaseTimeEntity {
+
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String title;
+
+  @Column(nullable = false)
+  private String content;
+
+  @Column(nullable = false)
+  private Long views = 0L; // 조회수 필드 생성 + 0으로 초기화
+
+}
