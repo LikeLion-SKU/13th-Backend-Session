78767181a25f71fb5de0f201f78f941fe30d5a91
simGPT <2023216057@skuniv.ac.kr>
2025-05-01 22:16:08 +0900

    :tada:Start: BaseTimeEntity 생성

---
+++ src/main/java/com/likelion/springpractice/global/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.likelion.springpractice.global;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+  @CreatedDate
+  private LocalDateTime createAt; // 게시글 생성 일시 필드
+
+  @LastModifiedDate
+  private LocalDateTime modifiedAt;
+
+}
