6abad86df573d4301fc4929a1562f25a95ff397b
simGPT <2023216057@skuniv.ac.kr>
2025-05-01 22:08:37 +0900

    :tada:Start: 응답DTO 클래스 생성

---
+++ src/main/java/com/likelion/springpractice/domain/post/week05/dto/response/PostResponse.java
@@ -0,0 +1,21 @@
+package com.likelion.springpractice.domain.post.week05.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@Schema(title = "PostResponse: 게시글 응답 DTO")
+public class PostResponse {
+
+  @Schema(description = "게시글 ID", example = "1")
+  private Long postId; // 요창 DTO 클래스에는 없음
+
+  @Schema(description = "게시글 제목", example = "5주차 세션")
+  private String title;
+
+  @Schema(description = "게시글 내용", example = "이번주 세션도 화이팅!")
+  private String content;
+
+}
