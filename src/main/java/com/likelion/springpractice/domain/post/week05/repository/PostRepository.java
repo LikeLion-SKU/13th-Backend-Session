e4133add5a21d557139af67c0cf6113319b14f85
naooung <1030n@naver.com>
2025-05-02 15:28:42 +0900

    :sparkles: Feat: 5주차 과제: PostRepository에 조회 많은순 및 최신순 정렬 메서드 추가

--- src/main/java/com/likelion/springpractice/domain/post/week05/repository/PostRepository.java
+++ src/main/java/com/likelion/springpractice/domain/post/week05/repository/PostRepository.java
@@ -7,5 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
   List<Post> findByTitleAndWriter(String title, String writer);
+
   List<Post> findTop2ByTitle(String title);
+
+  List<Post> findAllByOrderByViewCountDesc();
+
+  List<Post> findAllByOrderByCreatedAtDesc();
 }
