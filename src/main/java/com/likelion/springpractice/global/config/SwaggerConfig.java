724ea685bd758c60a44af3ea1328920697a37899
hamtorygoals <yhaemin0531@naver.com>
2025-05-07 11:19:03 +0900

    :truck:Rename: config 파일을 만들어 그 안에 옮김

--- src/main/java/com/likelion/springpractice/global/config/SwaggerConfig.java
+++ src/main/java/com/likelion/springpractice/global/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.likelion.springpractice.global;
+package com.likelion.springpractice.global.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
