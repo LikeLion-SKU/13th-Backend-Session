5ea014bab109bb05d256dffe3a1e6900cfdbfaed
hamtorygoals <yhaemin0531@naver.com>
2025-05-07 11:20:06 +0900

    :sparkles:Feat: 6주차 세션 추가

---
+++ src/main/java/com/likelion/springpractice/global/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.likelion.springpractice.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+  private final CorsConfig corsConfig;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity
+        //CORS 설정
+        .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
+        //CSRF 보호 기능 비활성화 (REST API에서는 필요 없음)
+        .csrf(CsrfConfigurer::disable)
+        //HTTP 요청에 대한 권한 설정
+        .authorizeHttpRequests(
+            auth ->
+                auth.requestMatchers(
+                        "/swagger-ui/**",
+                        "/swagger-ui.html",
+                        "/v3/api-docs/**")
+                    .permitAll()
+                    //그 외 모든 요청 허용
+                    .anyRequest()
+                    .permitAll())
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .httpBasic(Customizer.withDefaults());
+
+    return httpSecurity.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
