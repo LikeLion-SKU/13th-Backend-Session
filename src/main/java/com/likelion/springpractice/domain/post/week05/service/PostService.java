82afd179d3c99c5033380f0faf088ea2b58d63f2
SunSeoYeon <ssy01157@gmail.com>
2025-05-13 09:26:41 +0900

    :sparkles:PostService 수정

--- src/main/java/com/likelion/springpractice/domain/post/week05/service/PostService.java
+++ src/main/java/com/likelion/springpractice/domain/post/week05/service/PostService.java
@@ -8,10 +8,12 @@ import com.likelion.springpractice.domain.post.week05.repository.PostRepository;
 import jakarta.transaction.Transactional;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PostService {
 
   private final PostRepository postRepository;
@@ -19,6 +21,8 @@ public class PostService {
 
   @Transactional
   public PostResponse createPost(CreatePostRequest createPostRequest) {
+    log.info("[서비스] 게시글 생성 시도:title={},content={}", createPostRequest.getTitle(),
+        createPostRequest.getContent());
     Post post = Post.builder()
         .title(createPostRequest.getTitle())
         .content(createPostRequest.getContent())
@@ -26,29 +30,44 @@ public class PostService {
         .build();
 
     postRepository.save(post);
+    log.info("[서비스]게시글 생성 완료: id={},title={},content={}", post.getId(), post.getTitle(),
+        post.getContent());
     return toPostResponse(post);
   }
 
 
   public List<PostResponse> getAllPosts() {
+    log.info("[서비스] 게시글 전체 조회 시도");
     List<Post> postList = postRepository.findAll();
+    log.info("[서비스] 조회된 게시글 수: {}", postList.size());
     return postList.stream().map(this::toPostResponse).toList();
   }
 
   //5주차에 transactional추가함
   @Transactional
   public PostResponse getPostById(Long id) {
+    log.info("[서비스] 게시글 단일 조회 시도: id={}", id);
     Post post = postRepository.findById(id)
-        .orElseThrow(() -> new IllegalArgumentException("게시글을 찾을수없습니다."));
+        .orElseThrow(() -> {
+          log.warn("[서비스] 게시글 조회 실패 -존재하지않음: id={}", id);
+
+          return new IllegalArgumentException("게시글을 찾을수없습니다.");
+        });
 //    5주차때 조회수 증가 할수있는거넣음
+    log.info("[서비스] 게시글 단일 조회 성공: id={}", id);
     post.plusViewCount();
     return toPostResponse(post);
   }
 
   @Transactional
   public PostResponse updatePost(Long id, UpdatePostRequest updatePostRequest) {
+    log.info("[서비스] 게시글 수정 시도: id={},newTitle={},newContent={}", id, updatePostRequest.getTitle(),
+        updatePostRequest.getContent());
     Post post = postRepository.findById(id)
-        .orElseThrow(() -> new IllegalArgumentException("게시글을 찾을 수 없습니다."));
+        .orElseThrow(() -> {
+          log.warn("[서비스] 게시글 수정 실패 -존재하지않음: id={}", id);
+          return new IllegalArgumentException("게시글을 찾을 수 없습니다.");
+        });
 
     post.update(updatePostRequest.getTitle(), updatePostRequest.getContent());
 
@@ -57,8 +76,12 @@ public class PostService {
 
   @Transactional
   public Boolean deletePost(Long id) {
+    log.info("[서비스] 게시글 삭제 시도: id={}", id);
     Post post = postRepository.findById(id)
-        .orElseThrow(() -> new IllegalArgumentException("게시글을 찾을수 없습니다."));
+        .orElseThrow(() -> {
+          log.warn("[서비스] 게시글 삭제 실패 - 존재하지 않음: id={}", id);
+          return new IllegalArgumentException("게시글을 찾을수 없습니다.");
+        });
     postRepository.deleteById(id);
     return true;
   }
