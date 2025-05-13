375ab9611dc4990c1ccc05380fbad7f69f8b06f5
shinchaerin79 <shinchaerin79@gmail.com>
2025-05-13 12:36:24 +0900

    :sparkles: Feat: 로그 레벨 테스트용 LoggingController 추가

---
+++ src/main/java/com/likelion/springpractice/domain/post/week07/LoggingController.java
@@ -0,0 +1,22 @@
+package com.likelion.springpractice.domain.post.week07;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class LoggingController {
+
+  @GetMapping("/api/log-test")
+  public String logTest() {
+    log.trace("Trace Level 로그입니다 - 가장 상세한 정보");
+    log.debug("Debug Level 로그립니다 - 디버깅용");
+    log.info("Info Level 로그입니다 - 일반 정보");
+    log.warn("Warn Level 로그입니다 - 경고");
+    log.error("Error Level 로그립니다 - 오류 발생");
+
+    return "로그 테스트 완료!";
+  }
+
+}
