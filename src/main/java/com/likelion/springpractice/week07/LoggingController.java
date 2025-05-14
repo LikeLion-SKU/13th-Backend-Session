f244224ea4573a24764c6329a5e45144ce307558
pjuyong <imjuyongp@naver.com>
2025-05-14 18:05:18 +0900

    ✨ Feat: LoggingController 추가

---
+++ src/main/java/com/likelion/springpractice/week07/LoggingController.java
@@ -0,0 +1,21 @@
+package com.likelion.springpractice.week07;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j // Logger 객체 자동 생성
+public class LoggingController {
+
+  @GetMapping("/api/log-test")
+  public String logTest() {
+    log.trace("Trace Level 로그입니다 - 가장 상세한 정보"); // 출력 x
+    log.debug("Debug Level 로그입니다 - 디버깅용"); // 출력 x
+    log.info("Info Level 로그입니다 - 일반 정보");
+    log.warn("Warn Level 로그입니다 - 경고");
+    log.error("Error Level 로그입니다 - 오류 발생");
+
+    return "로그 테스트 완료";
+  }
+}
