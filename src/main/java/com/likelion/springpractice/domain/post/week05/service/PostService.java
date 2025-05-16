72f2c5f824785add315403eba6fd93633c3cbd09
chaeelin <chaeee0123@gmail.com>
2025-05-16 10:29:19 +0900

    :loud_sound: Log: 로그 추가

--- src/main/java/com/likelion/springpractice/domain/post/week05/service/PostService.java
+++ src/main/java/com/likelion/springpractice/domain/post/week05/service/PostService.java
@@ -8,44 +8,60 @@ import com.likelion.springpractice.domain.post.week05.repository.PostRepository;
 import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PostService {
   private final PostRepository postRepository;
 
   // 게시글 생성
   @Transactional
   public PostResponse createPost(CreatePostRequest createPostRequest) {
+    log.info("[서비스]게시글 생성 시도: title= {}, content= {}", createPostRequest.getTitle()
+        , createPostRequest.getContent());
     Post post = Post.builder()
         .title(createPostRequest.getTitle())
         .content(createPostRequest.getContent())
         .build();
     postRepository.save(post);
-
+    log.info("[서비스]게시글 생성 완료: id={}, title= {}, content= {}", post.getId(), createPostRequest.getTitle()
+        , createPostRequest.getContent());
     return toPostResponse(post);
   }
 
   // 게시글 전체 조회
   public List<PostResponse> getAllPosts() {
+    log.info("[서비스] 게시글 전체 조회 시도");
     List<Post> postList = postRepository.findAll();
+    log.info("[서비스] 조회된 게시글 수: {}", postList.size());
     return postList.stream().map(this::toPostResponse).toList();
   }
 
   // 게시글 단일 조회
   public PostResponse getPostById(Long id) {
+    log.info("[서비스] 게시글 단일 조회 시도: id={}", id);
     Post post = postRepository.findById(id)
-        .orElseThrow(() -> new IllegalArgumentException("게시글을 찾을 수 없습니다"));
+        .orElseThrow(() -> {
+          log.warn("[서비스] 게시글 조회 실패 - 존재하지 않음: id={}", id);
+          return new IllegalArgumentException("게시글을 찾을 수 없습니다");
+        });
      return toPostResponse(post);
   }
 
   // 게시글 수정
   @Transactional
   public PostResponse updatePost(Long id, UpdatePostRequest updatePostRequest) {
+    log.info("[서비스] 게시글 수정 시도: id={}, newTitle={}, newContent= {}", id,
+        updatePostRequest.getTitle(), updatePostRequest.getContent());
     Post post = postRepository.findById(id)
-        .orElseThrow(() -> new IllegalArgumentException("게시글을 찾을 수 없습니다."));
+        .orElseThrow(() -> {
+          log.warn("[서비스] 게시글 수정 실패 - 존재하지 않음 id={}", id);
+          return new IllegalArgumentException("게시글을 찾을 수 없습니다.");
+        });
 
     Post updatedPost = Post.builder()
         .id(post.getId())
@@ -54,16 +70,24 @@ public class PostService {
         .build();
     postRepository.save(updatedPost);
 
+    log.info("[서비스] 게시글 수정 완료: id={}, title={}, content={}", post.getId(),
+        updatePostRequest.getTitle(), updatePostRequest.getContent());
     return toPostResponse(updatedPost);
   }
 
   //개사글 삭제
   @Transactional
   public Boolean deletePost(Long id) {
+    log.info("[서비스] 게시글 삭제 시도: id={}", id);
+
     Post post = postRepository.findById(id)
-        .orElseThrow(() -> new IllegalArgumentException("게시글을 찾을 수 없습니다."));
+        .orElseThrow(() -> {
+          log.warn("[서비스] 게시글 삭제 실패 - 조내잫지 않음: id={}", id);
+          return new IllegalArgumentException("게시글을 찾을 수 없습니다.");
+        });
 
     postRepository.deleteById(id);
+    log.info("[서비스] 게시글 삭제 완료: id={}", id);
     return true;
   }
 
