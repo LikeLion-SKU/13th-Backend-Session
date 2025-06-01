0db34200f7f75816322f55394d2de18d36d8162a
hyunseoko <kohyun0223@skuniv.ac.kr>
2025-06-01 14:41:28 +0900

    :sparkles:Feat: JWT 설정, 로그인/회원가입

---
+++ src/main/java/com/likelion/springpractice/global/exception/CustomException.java
@@ -0,0 +1,15 @@
+package com.likelion.springpractice.global.exception;
+
+import com.likelion.springpractice.global.exception.model.BaseErrorCode;
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+  private final BaseErrorCode errorCode;
+
+  public CustomException(BaseErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.errorCode = errorCode;
+  }
+}
