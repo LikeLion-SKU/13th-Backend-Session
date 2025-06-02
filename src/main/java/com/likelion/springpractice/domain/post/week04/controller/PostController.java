1eda5c49909593393a3d8333186b874bcca55db8
이채민 <chaemin@ichaemin-ui-MacBookAir.local>
2025-06-02 19:19:39 +0900

    :sparkles:Feat: 10주차 세션

--- src/main/java/com/likelion/springpractice/domain/post/week04/controller/PostController.java
+++ src/main/java/com/likelion/springpractice/domain/post/week04/controller/PostController.java
@@ -4,9 +4,11 @@ import com.likelion.springpractice.domain.post.week05.dto.request.CreatePostRequ
 import com.likelion.springpractice.domain.post.week05.dto.request.UpdatePostRequest;
 import com.likelion.springpractice.domain.post.week05.dto.response.PostResponse;
 import com.likelion.springpractice.domain.post.week05.service.PostService;
+import com.likelion.springpractice.global.response.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -28,11 +30,13 @@ public class PostController {
 
   private final PostService postService;
 
-  @Operation(summary = "게시글 생성",
-    description = "게시판 페이지에서 게시글 작성 후 생성 버튼을 눌렀을 때 요청되는 API")
+  @Operation(summary = "게시글 생성", description = "게시판 페이지에서 게시글 작성 후 생성 버튼을 눌렀을 때 요청되는 API")
   @PostMapping("/posts")
-  public ResponseEntity<PostResponse> createPost(@Parameter(description = "게시글 작성 내용") @RequestBody CreatePostRequest createPostRequest) {
-    return ResponseEntity.ok(postService.createPost(createPostRequest));
+  public ResponseEntity<BaseResponse<PostResponse>> createPost(
+          @Parameter(description = "게시글 작성 내용")
+          @RequestBody @Valid CreatePostRequest createPostRequest) {
+    PostResponse response = postService.createPost(createPostRequest);
+    return ResponseEntity.ok(BaseResponse.success("게시글 생성 성공", response));
   }
 
   @Operation(summary = "게시글 전체 조회",
--- src/main/java/com/likelion/springpractice/domain/post/week05/service/PostService.java
+++ src/main/java/com/likelion/springpractice/domain/post/week05/service/PostService.java
@@ -1,10 +1,12 @@
 package com.likelion.springpractice.domain.post.week05.service;
 
+import com.likelion.springpractice.domain.post.exception.PostErrorCode;
 import com.likelion.springpractice.domain.post.week04.entity.Post;
 import com.likelion.springpractice.domain.post.week05.dto.request.CreatePostRequest;
 import com.likelion.springpractice.domain.post.week05.dto.request.UpdatePostRequest;
 import com.likelion.springpractice.domain.post.week05.repository.PostRepository;
 import com.likelion.springpractice.domain.post.week05.dto.response.PostResponse;
+import com.likelion.springpractice.global.exception.CustomException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +25,19 @@ public class PostService {
     public PostResponse createPost(CreatePostRequest createPostRequest) {
         log.info("[서비스] 게시글 생성 시도: title= {}, content= {}", createPostRequest.getTitle(),
                 createPostRequest.getContent());
+
+        if (createPostRequest.getTitle() == null || createPostRequest.getTitle().isBlank()) {
+            throw new CustomException(PostErrorCode.INVALID_POST_TITLE);
+        }
+
+        if (createPostRequest.getContent() == null || createPostRequest.getContent().isBlank()) {
+            throw new CustomException(PostErrorCode.INVALID_POST_CONTENT);
+        }
+
+        if (createPostRequest.getContent().length() > 10) {
+            throw new CustomException(PostErrorCode.TITLE_TOO_LONG);
+        }
+
         Post post = Post.builder()
                 .title(createPostRequest.getTitle())
                 .content(createPostRequest.getContent())
