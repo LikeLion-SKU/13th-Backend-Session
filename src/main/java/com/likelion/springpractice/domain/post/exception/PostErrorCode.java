83156aa11c7cc3e012f82afb6c0718a1214c8be0
김나경 <1030n@naver.com>
2025-06-05 17:44:02 +0900

    :sparkles: Feat: 10주차 과제2 - PostErrorCode 추가

--- src/main/java/com/likelion/springpractice/domain/post/exception/PostErrorCode.java
+++ src/main/java/com/likelion/springpractice/domain/post/exception/PostErrorCode.java
@@ -12,7 +12,8 @@ public enum PostErrorCode implements BaseErrorCode {
   POST_NOT_FOUND("POST_4041", "헤딩 게시글을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
   INVALID_POST_TITLE("POST_4001", "게시글 제목은 필수입니다.", HttpStatus.BAD_REQUEST),
   INVALID_POST_CONTENT("POST_4002", "게시글 내용은 필수입니다.", HttpStatus.BAD_REQUEST),
-  TITLE_TOO_LONG("POST_4003", "게시글 제목은 10자 이하로 작성해야 합니다.", HttpStatus.BAD_REQUEST);
+  TITLE_TOO_LONG("POST_4003", "게시글 제목은 10자 이하로 작성해야 합니다.", HttpStatus.BAD_REQUEST),
+  NO_CHANGES_DETECTED("POST_4004", "게시글 변경이 없습니다.", HttpStatus.BAD_REQUEST);
 
   private final String code;
   private final String message;
