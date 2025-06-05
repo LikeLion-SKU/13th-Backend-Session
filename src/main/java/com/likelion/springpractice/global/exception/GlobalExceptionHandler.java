136bbcf35da1243e230546721606291b69679b62
SunSeoYeon <ssy01157@gmail.com>
2025-06-06 02:03:20 +0900

    :recycle:GlobalExceptionHandler 예외 핸들링 3개 추가

--- src/main/java/com/likelion/springpractice/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/likelion/springpractice/global/exception/GlobalExceptionHandler.java
@@ -2,10 +2,13 @@ package com.likelion.springpractice.global.exception;
 
 import com.likelion.springpractice.global.exception.model.BaseErrorCode;
 import com.likelion.springpractice.global.response.BaseResponse;
+import java.nio.file.AccessDeniedException;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -36,6 +39,32 @@ public class GlobalExceptionHandler {
     return ResponseEntity.badRequest().body(BaseResponse.error(400, errorMessages));
   }
 
+  //  JSON이 이상할 때
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<BaseResponse<Object>> handleInvalidJson(
+      HttpMessageNotReadableException ex) {
+    log.warn("요청 바디 파싱 실패: {}", ex.getMessage());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(BaseResponse.error(400, "요청 형식이 잘못되었습니다. JSON 형식을 확인해주세요."));
+  }
+
+  //권한이 없을때
+  @ExceptionHandler(AccessDeniedException.class)
+  public ResponseEntity<BaseResponse<Object>> handleAccessDenied(AccessDeniedException ex) {
+    log.warn("접근 거부 예외 발생: {}", ex.getMessage());
+    return ResponseEntity.status(HttpStatus.FORBIDDEN)
+        .body(BaseResponse.error(403, "접근 권한이 없습니다."));
+  }
+
+  // 비밀번호가 올바르지않을때
+  @ExceptionHandler(AuthenticationException.class)
+  public ResponseEntity<BaseResponse<Object>> handleAuthenticationException(
+      AuthenticationException ex) {
+    log.warn("인증 실패: {}", ex.getMessage());
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+        .body(BaseResponse.error(401, "비밀번호가 올바르지 않습니다."));
+  }
+
   // 예상치 못한 예외
   @ExceptionHandler(Exception.class)
   public ResponseEntity<BaseResponse<Object>> handleException(Exception ex) {
@@ -43,4 +72,6 @@ public class GlobalExceptionHandler {
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
         .body(BaseResponse.error(500, "예상치 못한 서버 오류가 발생했습니다."));
   }
+
+
 }
