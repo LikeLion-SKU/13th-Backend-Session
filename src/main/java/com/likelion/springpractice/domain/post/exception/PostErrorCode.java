822d31a93e9532c4861e61f414e54d53d59db540
SunSeoYeon <ssy01157@gmail.com>
2025-06-08 00:45:03 +0900

    :tada:PostErrorCode 파일 생성

---
+++ src/main/java/com/likelion/springpractice/domain/post/exception/PostErrorCode.java
@@ -0,0 +1,20 @@
+package com.likelion.springpractice.domain.post.exception;
+
+import com.likelion.springpractice.global.exception.model.BaseErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum PostErrorCode implements BaseErrorCode {
+  POST_ERROR_CODE("POST_4041", "해당 게시글을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+  INVALID_POST_TITLE("POST_4001", "게시글 제목은 필수입니다.", HttpStatus.BAD_REQUEST),
+  INVALID_POST_CONTENT("POST_4002", "게시글 내용은 필수입니다.", HttpStatus.BAD_REQUEST),
+  TITLE_TOO_LONG("POST_4003", "게시글 제목은 10자 이하로 작성해야 합니다.", HttpStatus.BAD_REQUEST);
+  private final String code;
+  private final String message;
+  private final HttpStatus status;
+
+
+}
