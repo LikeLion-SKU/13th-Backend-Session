902a557c742f391524e0baf70732563d573b5be0
SunSeoYeon <ssy01157@gmail.com>
2025-06-08 00:45:57 +0900

    :recycle:CreatePostRequest,UpdatePostRequest 수정

--- src/main/java/com/likelion/springpractice/domain/post/week05/dto/request/CreatePostRequest.java
+++ src/main/java/com/likelion/springpractice/domain/post/week05/dto/request/CreatePostRequest.java
@@ -1,14 +1,18 @@
 package com.likelion.springpractice.domain.post.week05.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 @Schema(title = "CreatePostRequest: 게시글 생성 요청 DTO")
 public class CreatePostRequest {
 
-  @Schema(description = "게시글 제목", example = "1주차 세션???")
+  @NotBlank(message = "제목은 비어 있을수없습니다.")
+  @Schema(description = "게시글 제목", example = "5주차")
   private String title;
+
+  @NotBlank(message = "내용은 비어있을 수 없습니다.")
   @Schema(description = "게시글 내용", example = "내용")
   private String content;
 
--- src/main/java/com/likelion/springpractice/domain/post/week05/dto/request/UpdatePostRequest.java
+++ src/main/java/com/likelion/springpractice/domain/post/week05/dto/request/UpdatePostRequest.java
@@ -1,14 +1,17 @@
 package com.likelion.springpractice.domain.post.week05.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 @Schema(title = "UpdatePostRequest: 게시글 수정 요청 DTO")
 public class UpdatePostRequest {
 
+  @NotBlank(message = "제목은 비어 있을수없습니다.")
   @Schema(description = "게시글 제목", example = "5주차 세션")
   private String title;
+  @NotBlank(message = "내용은 비어있을 수 없습니다.")
   @Schema(description = "게시글 내용", example = "이번주 세션도 화이팅!")
   private String content;
 
