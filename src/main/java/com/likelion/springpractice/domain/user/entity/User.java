0734244bd68486119e58f2a4da54b7f56157dd28
Comeoksal <keumsiun0503@gmail.com>
2025-06-09 20:32:47 +0900

    :recycle:Refactor: Update User Entity

--- src/main/java/com/likelion/springpractice/domain/user/entity/User.java
+++ src/main/java/com/likelion/springpractice/domain/user/entity/User.java
@@ -1,28 +1,38 @@
 package com.likelion.springpractice.domain.user.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.likelion.springpractice.domain.userbatch.entity.UserBatch;
 import com.likelion.springpractice.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@Table(name = "users")
+@Table(name = "user")
 public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "username", nullable = false)
-    private String username;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
 
     @JsonIgnore
     @Column(name = "password", nullable = false)
     private String password;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "introduce")
+    private String introduce;
 
     @JsonIgnore
     @Column(name = "refresh_token")
@@ -32,6 +42,14 @@ public class User extends BaseTimeEntity {
     @Enumerated(EnumType.STRING)
     @Builder.Default
     private Role role = Role.USER;
+
+    @Column(name = "country", nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private Country country = Country.KOREA;
+
+    @OneToMany(mappedBy = "user")
+    private List<UserBatch> userBatches = new ArrayList<>();
 
     public void createRefreshToken(String refreshToken) {
         this.refreshToken = refreshToken;
