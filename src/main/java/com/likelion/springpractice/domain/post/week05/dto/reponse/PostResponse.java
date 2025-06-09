3170446343873ce713e0efa4b7ed32cb38b939b3
pjuyong <imjuyongp@gmail.com>
2025-06-09 15:11:13 +0900

    ♻️ Refactor: BaseTimeEntity 상속x

--- src/main/java/com/likelion/springpractice/domain/post/week05/dto/reponse/PostResponse.java
+++ src/main/java/com/likelion/springpractice/domain/post/week05/dto/reponse/PostResponse.java
@@ -9,7 +9,7 @@ import lombok.Getter;
 @Getter
 @Builder // 빌더를 만들었기 때문에 생성자를 만들 필요가 없음
 @Schema(title = "PostResponse: 게시글 응답 DTO")
-public class PostResponse extends BaseTimeEntity {
+public class PostResponse {
 
   @Schema(description = "게시글 ID", example = "1")
   private Long postId;
@@ -19,9 +19,6 @@ public class PostResponse extends BaseTimeEntity {
 
   @Schema(description = "게시글 내용", example = "세션 화이팅")
   private String content;
-
-  @Schema(description = "게시글 생성 시간", example = "???")
-  private LocalDateTime createdAt;
 
   @Schema(description = "조회수")
   private Integer views;
