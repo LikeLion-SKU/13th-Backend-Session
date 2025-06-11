00c931209b0dd032ee68a8d4298aaca055d7ceaf
pjuyong <imjuyongp@gmail.com>
2025-06-11 16:53:29 +0900

    ♻️ Refactor: 회원, 비회원 역할 수정

--- src/main/java/com/likelion/springpractice/domain/user/entity/Role.java
+++ src/main/java/com/likelion/springpractice/domain/user/entity/Role.java
@@ -3,8 +3,10 @@ package com.likelion.springpractice.domain.user.entity;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public enum Role {
-  @Schema(description = "사용자")
-  USER,
+  @Schema(description = "회원")
+  MEMBER,
+  @Schema(description = "비회원")
+  NONMEMBER,
   @Schema(description = "관리자")
   ADMIN,
   @Schema(description = "개발자")
