da08a4a4ee88fbdc7dfafa9e6be84ec7ec047c0e
pjuyong <imjuyongp@gmail.com>
2025-06-14 15:44:31 +0900

    ♻️ Refactor: foodName unique true 설정

--- src/main/java/com/likelion/springpractice/domain/food/entity/Food.java
+++ src/main/java/com/likelion/springpractice/domain/food/entity/Food.java
@@ -1,12 +1,17 @@
 package com.likelion.springpractice.domain.food.entity;
 
+import com.likelion.springpractice.domain.review.entity.Review;
 import com.likelion.springpractice.global.BaseTimeEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,7 +29,7 @@ public class Food extends BaseTimeEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long foodId;
 
-  @Column(nullable = false)
+  @Column(nullable = false, unique = true)
   private String foodName;
 
   @Column(nullable = false)
@@ -38,6 +43,11 @@ public class Food extends BaseTimeEntity {
 
   @Column(nullable = false)
   private int reviewNum = 0; // 리뷰 수
+
+  // reviews 테이블과 연관 -> food는 review를 여러개 가짐
+  @OneToMany(mappedBy = "food", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<Review> reviews = new ArrayList<>();
+
 
   public void increaseLikeNum() { // 좋아요 수 증가
     likeNum++;
