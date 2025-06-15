95c57814f7f6ffb3128b0fca96e5be3b9a76e520
pjuyong <imjuyongp@gmail.com>
2025-06-16 01:37:02 +0900

    ♻️ Refactor: 리뷰 작성 DTO 수정

--- src/main/java/com/likelion/springpractice/domain/review/dto/request/CreateReviewRequest.java
+++ src/main/java/com/likelion/springpractice/domain/review/dto/request/CreateReviewRequest.java
@@ -8,6 +8,14 @@ import lombok.Getter;
 @Schema(title = "CreateReviewRequest: 리뷰 생성 요청 DTO")
 public class CreateReviewRequest {
 
+  @NotBlank(message = "사용자 이름은 필수입니다.")
+  @Schema(description = "사용자 이름", example = "주용")
+  private String username;
+
+  @NotBlank(message = "음식 ID는 필수입니다.")
+  @Schema(description = "음식 ID", example = "1")
+  private Long foodId;
+
   @NotBlank(message = "점수를 매겨주세요.")
   @Schema(description = "음식 점수", example = "3")
   private int score;
