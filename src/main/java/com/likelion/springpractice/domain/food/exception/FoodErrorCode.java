4e48303973ff5a3124940332c4447891112ec3b4
pjuyong <imjuyongp@gmail.com>
2025-06-16 01:23:55 +0900

    ♻️ Refactor: ErrorCode 수정

--- src/main/java/com/likelion/springpractice/domain/food/exception/FoodErrorCode.java
+++ src/main/java/com/likelion/springpractice/domain/food/exception/FoodErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum FoodErrorCode implements BaseErrorCode {
-  FOOD_ERROR_CODE("POST_4041", "해당 게시글을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+  FOOD_NOT_FOUND("FOOD_4041", "해당 음식을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+
 
   private final String code;
   private final String message;
