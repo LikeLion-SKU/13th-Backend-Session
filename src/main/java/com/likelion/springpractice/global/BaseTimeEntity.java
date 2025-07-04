5e60f5b92bb251d563c0eaf88f2d97a0ece963b1
simGPT <2023216057@skuniv.ac.kr>
2025-07-04 21:54:08 +0900

    :card_file_box:Comment: BaseTimeEntity 주석 추가

--- src/main/java/com/likelion/springpractice/global/BaseTimeEntity.java
+++ src/main/java/com/likelion/springpractice/global/BaseTimeEntity.java
@@ -1,5 +1,6 @@
 package com.likelion.springpractice.global;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
@@ -14,7 +15,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public abstract class BaseTimeEntity {
 
   @CreatedDate
-  private LocalDateTime createAt; // 게시글 생성 일시 필드
+  @Column(updatable = false)
+  private LocalDateTime createdAt; // 게시글 생성 일시 필드
 
   @LastModifiedDate
   private LocalDateTime modifiedAt;
