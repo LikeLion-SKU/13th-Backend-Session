f54d6939d0ed7e17cfe4798226ba5882d2fa4cd4
김나경 <1030n@naver.com>
2025-07-04 17:26:49 +0900

    :sparkles: Feat: Badge Domain 추가

---
+++ src/main/java/com/likelion/springpractice/domain/Badge/entity/Badge.java
@@ -0,0 +1,35 @@
+package com.likelion.springpractice.domain.Badge.entity;
+
+import com.likelion.springpractice.global.common.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "badge")
+public class Badge extends BaseTimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Column(name = "description", nullable = false)
+  private String description;
+
+  @Column(name = "unlock_count", nullable = false)
+  private Integer unlock_count;
+}
