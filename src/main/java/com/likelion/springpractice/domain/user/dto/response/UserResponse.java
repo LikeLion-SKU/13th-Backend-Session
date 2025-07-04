b0f687b2fc333de8d944ff043469a83c0fb47323
simGPT <2023216057@skuniv.ac.kr>
2025-07-04 21:45:08 +0900

    :tada:Start: UserResponse 생성

--- src/main/java/com/likelion/springpractice/domain/user/dto/response/UserResponse.java
+++ src/main/java/com/likelion/springpractice/domain/user/dto/response/UserResponse.java
@@ -1,4 +1,15 @@
 package com.likelion.springpractice.domain.user.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@Schema(title = "UserResponse DTO", description = "사용자 회원 정보 수정에 대한 응답 변환")
 public class UserResponse {
+
+  private final String name;
+  private final String nationality;
+
 }
